14b8bedeaddc2eab62f9ab0a7d1bbe43ea67bd22
Mikulas Lukacs <mikulaslukacs1@gmail.com>
2023-07-15 19:28:43 +0100

    Update metadata.js

--- _data/metadata.js
+++ _data/metadata.js
@@ -4,8 +4,8 @@ module.exports = {
 	language: "en",
 	description: "I am writing about my experiences as a naval navel-gazer.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
+		name: "Mikulas Lukacs",
+		email: "mikulaslukacs1@gmail.com",
 		url: "https://example.com/about-me/"
 	}
 }
